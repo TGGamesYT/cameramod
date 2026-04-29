d0168426ec69c87970fc92097bae1d74ed349933
TGGamesYT <tothgergoci@gmail.com>
2026-04-29 16:19:04 +0200

    delete accidental src/src

--- src/src/client/java/me/tg/cameramod/client/CameraEntityRenderState.java
+++
@@ -1,8 +0,0 @@
-package me.tg.cameramod.client;
-
-import net.minecraft.client.render.entity.state.LivingEntityRenderState;
-
-public class CameraEntityRenderState extends LivingEntityRenderState {
-    public boolean showLegs = true;
-    public float zoomLevel = 1.0f;
-}
